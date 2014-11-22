492ed42313624473f355bd595a375679c7d73be5
MrRoboman4321 <benevedeselias@gmail.com>
2014-11-21 16:28:20 -0800

    Query fix

--- savedata.php
+++ savedata.php
@@ -33,7 +33,7 @@
 
     if(!$curQuery) die("Table check/create failed!"); //if query failed, die
 
-    $curQuery = pg_query("INSERT INTO scouting (tablet, team, startingposition, autoshort, automedium, atuolarge, autocenter, kickstand, teleshort, telemedium, telelarge, endsmall, endmedium, endlarge, endcenter) VALUES (${tablet}, ${team}, ${startingposition}, ${autoshort}, ${automedium}, ${autolarge}, ${autocenter}, ${kickstand}, ${teleshort}, ${telemedium}, ${telelarge}, ${endsmall}, ${endmedium}, ${endlarge}, ${endcenter})");
+    $curQuery = pg_query("INSERT INTO scouting (tablet, team, startingposition, autoshort, automedium, autolarge, autocenter, kickstand, teleshort, telemedium, telelarge, endsmall, endmedium, endlarge, endcenter) VALUES (${tablet}, ${team}, ${startingposition}, ${autoshort}, ${automedium}, ${autolarge}, ${autocenter}, ${kickstand}, ${teleshort}, ${telemedium}, ${telelarge}, ${endsmall}, ${endmedium}, ${endlarge}, ${endcenter})");
 	
     if(!$curQuery) die("Data insert failed!"); //if query failed, die
 
