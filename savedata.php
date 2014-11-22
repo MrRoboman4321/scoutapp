215203bd79d9936c155efa6e954caaa665c2bf21
MrRoboman4321 <benevedeselias@gmail.com>
2014-11-21 19:10:33 -0800

    Added automoved and endmoved

--- savedata.php
+++ savedata.php
@@ -12,6 +12,7 @@
 	$automedium = $_POST["automedium"];
 	$autolarge = $_POST["autolarge"];
 	$autocenter = $_POST["autocenter"];
+    $automoved = $_POST["automoved"];
         //$autoparked = $_POST["autoparked"];
     $kickstand = $_POST["kickstand"];
         //$telestyle = $_POST["telestyle"];
@@ -22,6 +23,7 @@
 	$endmedium = $_POST["endmedium"];
 	$endlarge = $_POST["endlarge"];
     $endcenter = $_POST["endcenter"];
+    $endmoved = $_POST["endmoved"];
 	    //$endparked = $_POST["endparked"];
 
     $db = pg_connect('host=frc.nelsonnwalaska.com port=5432 user=postgres password=FrcAviation444! dbname=scouting');
@@ -29,11 +31,11 @@
     if(!$db) die("Failed to connect to the DB!"); //if we don't have a DB conn, die
 
 
-    $curQuery = pg_query("CREATE TABLE IF NOT EXISTS scouting(match INTEGER, team INTEGER, startingposition TEXT, autoshort INTEGER, automedium INTEGER, autolarge INTEGER, autocenter INTEGER, kickstand BOOL, teleshort INTEGER, telemedium INTEGER, telelarge INTEGER, endsmall INTEGER, endmedium INTEGER, endlarge INTEGER, endcenter INTEGER)");
+    $curQuery = pg_query("CREATE TABLE IF NOT EXISTS scouting(match INTEGER, team INTEGER, startingposition TEXT, autoshort INTEGER, automedium INTEGER, autolarge INTEGER, autocenter INTEGER, automoved INTEGER, kickstand BOOL, teleshort INTEGER, telemedium INTEGER, telelarge INTEGER, endsmall INTEGER, endmedium INTEGER, endlarge INTEGER, endcenter INTEGER, endmoved INTEGER)");
 
     if(!$curQuery) die("Table check/create failed!"); //if query failed, die
 
-    $curQuery = pg_query("INSERT INTO scouting (match, team, startingposition, autoshort, automedium, autolarge, autocenter, kickstand, teleshort, telemedium, telelarge, endsmall, endmedium, endlarge, endcenter) VALUES (${match}, ${team}, '${startingposition}', ${autoshort}, ${automedium}, ${autolarge}, ${autocenter}, ${kickstand}, ${teleshort}, ${telemedium}, ${telelarge}, ${endsmall}, ${endmedium}, ${endlarge}, ${endcenter})");
+    $curQuery = pg_query("INSERT INTO scouting (match, team, startingposition, autoshort, automedium, autolarge, automoved, autocenter, kickstand, teleshort, telemedium, telelarge, endsmall, endmedium, endlarge, endcenter, endmoved) VALUES (${match}, ${team}, '${startingposition}', ${autoshort}, ${automedium}, ${autolarge}, ${autocenter}, ${automoved}, ${kickstand}, ${teleshort}, ${telemedium}, ${telelarge}, ${endsmall}, ${endmedium}, ${endlarge}, ${endcenter}, ${endmoved})");
 	
     if(!$curQuery) die("Data insert failed!"); //if query failed, die
 
